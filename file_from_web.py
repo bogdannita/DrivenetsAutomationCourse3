fcc1ec6d5586a107052a132669b546f5e0c9e664
Bogdan Nita <bogdannita@gmail.com>
2026-01-03 00:06:26 +0200

    added encoding to the open file method

--- file_from_web.py
+++ file_from_web.py
@@ -1,6 +1,6 @@
 import requests
 
-f = open("frankenstein.txt", "w")  # w stands for open file for writing
+f = open("frankenstein.txt", "w", encoding='utf-8')  # w stands for open file for writing
 r = requests.get("https://www.gutenberg.org/cache/epub/84/pg84.txt")
 # print(r.content.decode())
 f.write(r.content.decode())
@@ -13,13 +13,13 @@ f.close()
 
 # count how many times the letter "a" shows up in the book
 num_a = 0
-f = open("frankenstein2.txt")
+f = open("frankenstein2.txt", encoding='utf-8')
 for line in f:
     num_a += line.count("a")
 print(f"The number of times a shows up in the book is {num_a}")
 f.close()
 
-f = open("frankenstein2.txt")
+f = open("frankenstein2.txt", encoding='utf-8')
 num_words = 0
 for line in f:
     words = line.split()
